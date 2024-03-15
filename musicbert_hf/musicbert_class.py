ec03a4b9f74fc934d030fdf0825010ed1f606bc6
Malcolm Sailor <malcolm.sailor@gmail.com>
2024-03-15 10:04:44 -0400

    wip

--- musicbert_hf/musicbert_class.py
+++ musicbert_hf/musicbert_class.py
@@ -24,132 +24,6 @@ from torch.nn import CrossEntropyLoss
 logger = logging.get_logger(__name__)
 
 
-# TODO: (Malcolm 2024-03-13) find the HF equivalent class to fairseq TransformerSentenceEncoder
-# TODO: (Malcolm 2024-03-13) do we need to derive this class from something more specific?
-# TODO: (Malcolm 2024-03-13) it looks like this functionality is divided between
-#   BertEmbeddings and BertEncoder; we mainly want to override the former
-class OctupleEncoder(nn.Module):
-    def __init__(
-        self,
-        *args,
-        embedding_dim: int,
-        compound_ratio: int = 8,
-        upsample: bool = True,
-        **kwargs,
-    ) -> None:
-        raise NotImplementedError
-        # TODO: (Malcolm 2024-03-13) super?
-        super().__init__(*args, **kwargs)
-        self.tpu = False
-        self.compound_ratio = compound_ratio
-        self.downsampling = nn.Sequential(
-            nn.Linear(embedding_dim * compound_ratio, embedding_dim)
-        )
-        self.upsample = upsample
-        # (Malcolm 2023-09-07) if `self.upsample` is False, then we shouldn't use
-        #   self.upsampling, but we nevertheless create it so that the model will
-        #   match the pretrained checkpoints. (We could hack `fairseq` so that
-        #   strict=False; there doesn't seem to be a cmd line arg for that.)
-        self.upsampling = nn.Sequential(
-            nn.Linear(embedding_dim, embedding_dim * compound_ratio)
-        )
-
-    def forward(
-        self,
-        tokens: torch.Tensor,
-        segment_labels: Optional[torch.Tensor] = None,
-        last_state_only: bool = False,
-        positions: Optional[torch.Tensor] = None,
-        token_embeddings: Optional[torch.Tensor] = None,
-    ) -> Union[
-        Tuple[torch.Tensor, torch.Tensor], Tuple[List[torch.Tensor], torch.Tensor]
-    ]:
-        raise NotImplementedError
-        # tokens: batch * compound_seq
-        assert tokens.ndim == 2
-
-        batch, compound_seq = tokens.shape
-        ratio = self.compound_ratio
-
-        seq = compound_seq / ratio
-
-        assert (
-            tokens.shape[1] % ratio == 0
-        ), f"token sequences length should be multiple of {ratio} for compound mode"
-        # (Malcolm 2024-02-20) I'm not sure what the motivation for this assertion
-        #   is, hidden states for intermediate layers seem to work as normal.
-        # assert last_state_only, "hidden states not available for compound mode"
-        assert positions is None, "custom positions are not supported for compound mode"
-        assert (
-            token_embeddings is None
-        ), "custom token embeddings are not supported for compound mode"
-        assert (
-            segment_labels is None
-        ), "segment embedding not supported for compound mode"
-
-        # # padding mask: boolean tensor (batch, seq)
-        # #   where seq = compound_seq // 8
-        # padding_mask = tokens[:, ::ratio].eq(self.padding_idx)
-        # assert padding_mask.shape == (batch, seq)
-
-        # # TODO: (Malcolm 2024-03-13) have a look at this
-        # if not self.traceable and not self.tpu and not padding_mask.any():
-        #     # ?
-        #     padding_mask = None
-
-        # TODO: (Malcolm 2024-03-13) implement embed_tokens
-        # x: batch, compound_seq, embedding_dim
-        x = self.embed_tokens(tokens)
-
-        # assert x.shape == (batch, compound_seq, self.embedding_dim)
-
-        # Project from (batch, compound_seq, embedding) -> (batch, seq, ratio * embedding )
-
-        unflattened = x.view(x.shape[0], x.shape[1] // ratio, -1)
-        x = self.downsampling(unflattened)
-
-        # assert x.shape == (batch, seq, ratio * self.embedding_dim)
-
-        # Add positional embeddings
-        # TODO: (Malcolm 2024-03-13)
-        if self.embed_positions is not None:
-            x = x + self.embed_positions(tokens[:, ::ratio], positions=positions)
-
-        # done
-        if self.emb_layer_norm is not None:
-            x = self.emb_layer_norm(x)
-
-        # done
-        x = self.dropout_module(x)
-
-        # TODO: (Malcolm 2024-03-13)
-        if padding_mask is not None:
-            x = x * (1 - padding_mask.unsqueeze(-1).type_as(x))
-
-        x = x.transpose(0, 1)
-        inner_states = []
-        if not last_state_only:
-            inner_states.append(x)
-        for layer in self.layers:
-            x, _ = layer(x, self_attn_padding_mask=padding_mask)
-            if not last_state_only:
-                inner_states.append(x)
-        if self.upsample:
-            x = x.transpose(0, 1)
-            x = self.upsampling(x).view(x.shape[0], x.shape[1] * ratio, -1)
-            x = x.transpose(0, 1)
-        sentence_rep = x[0, :, :]
-        if last_state_only:
-            inner_states = [x]
-        if self.traceable:
-            return torch.stack(inner_states), sentence_rep
-        else:
-            return inner_states, sentence_rep
-
-
-# TODO: (Malcolm 2024-03-15) set padding_idx to 1 throughout to match fairseq
-
-
 class CompoundEmbeddings(BertEmbeddings):
     """
     Closely modeled on `BertEmbeddings` class. We don't subclass because we don't
@@ -163,11 +37,9 @@ class CompoundEmbeddings(BertEmbeddings):
             nn.Linear(config.hidden_size * compound_ratio, config.hidden_size)
         )
         self.upsample = upsample
-        # TODO: (Malcolm 2024-03-13) revise following comment (do we ever use upsampling?)
-        # (Malcolm 2023-09-07) if `self.upsample` is False, then we shouldn't use
+        # (Malcolm 2023-09-07) if `self.upsample` is False, we won't ever use
         #   self.upsampling, but we nevertheless create it so that the model will
-        #   match the pretrained checkpoints. (We could hack `fairseq` so that
-        #   strict=False; there doesn't seem to be a cmd line arg for that.)
+        #   match the pretrained checkpoints.
         self.upsampling = nn.Sequential(
             nn.Linear(config.hidden_size, config.hidden_size * compound_ratio)
         )
@@ -177,34 +49,6 @@ class CompoundEmbeddings(BertEmbeddings):
         self.position_embeddings = nn.Embedding(
             config.max_position_embeddings, config.hidden_size, padding_idx=1
         )
-        # self.position_embeddings = nn.Embedding(
-        #     config.max_position_embeddings, config.hidden_size
-        # )
-        # self.token_type_embeddings = nn.Embedding(
-        #     config.type_vocab_size, config.hidden_size
-        # )
-
-        # TODO: (Malcolm 2024-03-14) remove I think, based on misunderstanding
-        # # Below taken directly from BertEmbeddings
-
-        # # self.LayerNorm is not snake-cased to stick with TensorFlow model variable name and be able to load
-        # # any TensorFlow checkpoint file
-        # self.LayerNorm = nn.LayerNorm(config.hidden_size, eps=config.layer_norm_eps)
-        # self.dropout = nn.Dropout(config.hidden_dropout_prob)
-        # # position_ids (1, len position emb) is contiguous in memory and exported when serialized
-        # self.position_embedding_type = getattr(
-        #     config, "position_embedding_type", "absolute"
-        # )
-        # self.register_buffer(
-        #     "position_ids",
-        #     torch.arange(config.max_position_embeddings).expand((1, -1)),
-        #     persistent=False,
-        # )
-        # self.register_buffer(
-        #     "token_type_ids",
-        #     torch.zeros(self.position_ids.size(), dtype=torch.long),
-        #     persistent=False,
-        # )
 
     def forward(
         self,
@@ -250,10 +94,9 @@ class CompoundEmbeddings(BertEmbeddings):
             "token_type_ids are not supported for compound mode"
         )
 
-        # TODO: (Malcolm 2024-03-13) it seems that the padding mask is not defined
-        #   in this module; find where I should define it.
-        #   In fact, it seems it is meant to be computed prior to calling BertModel
-        #   and provided as the attention_mask argument.
+        # TODO: (Malcolm 2024-03-13) rather than being implemented here, the padding
+        #   mask is meant to be computed prior to calling BertModel and provided
+        #   as the attention_mask argument.
 
         # padding_mask = input_ids[:, ::ratio].eq(self.padding_idx)
         # assert padding_mask.shape == (batch, seq)
@@ -286,8 +129,6 @@ class MusicBertEncoder(BertModel):
         super().__init__(config, add_pooling_layer=add_pooling_layer)
         self.config = config
         self.embeddings = CompoundEmbeddings(config)
-        # TODO: (Malcolm 2024-03-13) I think we can probably just use the BertEncoder
-        #   class without subclassing it
         self.encoder = BertEncoder(config)
         self.compound_ratio = compound_ratio
 
@@ -342,21 +183,21 @@ class MusicBertEncoder(BertModel):
             return_dict=return_dict,
         )
         if not return_dict:
-            # TODO: (Malcolm 2024-03-13)
             raise NotImplementedError
         else:
-            # TODO: (Malcolm 2024-03-13) should this in fact be a dictionary?
             output.last_hidden_state = self.embeddings.possibly_upsample(  # type:ignore
                 output.last_hidden_state  # type:ignore
             )
         return output
 
 
-# @add_start_docstrings(
-#     """Bert Model with a `language modeling` head on top.""", BERT_START_DOCSTRING
-# )
+@add_start_docstrings(
+    """Bert Model with a `language modeling` head on top.""", BERT_START_DOCSTRING
+)
 class MusicBert(BertPreTrainedModel):
-    # TODO: (Malcolm 2024-03-12) _tied_weights_keys = ["predictions.decoder.bias", "cls.predictions.decoder.weight"]
+    # (Malcolm 2024-03-12) BertForMaskedLM defines the following attributes. However,
+    #   tied weights are not implemented for MusicBert.
+    # _tied_weights_keys = ["predictions.decoder.bias", "cls.predictions.decoder.weight"]
 
     def __init__(self, config):
         super().__init__(config)
@@ -366,9 +207,11 @@ class MusicBert(BertPreTrainedModel):
                 "If you want to use `MusicBert` make sure `config.is_decoder=False` for "
                 "bi-directional self-attention."
             )
+        assert (
+            not config.tie_word_embeddings
+        ), "`tie_word_embeddings` is not implemented"
 
         self.bert = MusicBertEncoder(config, add_pooling_layer=False)
-        # TODO: (Malcolm 2024-03-14) verify
         self.cls = BertOnlyMLMHead(config)
 
         # Initialize weights and apply final processing
@@ -382,16 +225,9 @@ class MusicBert(BertPreTrainedModel):
         # TODO: (Malcolm 2024-03-14) verify
         self.cls.predictions.decoder = new_embeddings
 
-    # @add_start_docstrings_to_model_forward(
-    #     BERT_INPUTS_DOCSTRING.format("batch_size, sequence_length")
-    # )
-    # @add_code_sample_docstrings(
-    #     checkpoint=_CHECKPOINT_FOR_DOC,
-    #     output_type=MaskedLMOutput,
-    #     config_class=_CONFIG_FOR_DOC,
-    #     expected_output="'paris'",
-    #     expected_loss=0.88,
-    # )
+    @add_start_docstrings_to_model_forward(
+        BERT_INPUTS_DOCSTRING.format("batch_size, sequence_length")
+    )
     def forward(
         self,
         input_ids: Optional[torch.Tensor] = None,
@@ -437,6 +273,8 @@ class MusicBert(BertPreTrainedModel):
 
         masked_lm_loss = None
         if labels is not None:
+            # TODO: (Malcolm 2024-03-15) I think we want to set padding index
+            #   to 1 to match MusicBert implementation
             loss_fct = CrossEntropyLoss()  # -100 index = padding token
             masked_lm_loss = loss_fct(
                 prediction_scores.view(-1, self.config.vocab_size), labels.view(-1)
